4c8812fe41b6ba64510703d810d33b24749e2dcd
John Gilik <john@jgilik.com>
2015-01-09 22:42:13 -0800

    gitzebo: Configure SSH server to start, allowing git push / git pull to work.

--- pkgspecs/env-gitzebo.deps.sh
+++ pkgspecs/env-gitzebo.deps.sh
@@ -3,5 +3,10 @@ set -Eeo pipefail
 
 echo git # can't export git dep in Python :(
 echo python-gitzebo # configured module
+
+echo rootfiles # .bashrc needed for HOME and the like
+
 #echo httpd # serve using Apache
 #echo mod_wsgi # WSGI used to link to Python
+
+echo openssh-server # needed to accept pushes and serve pulls
--- pkgspecs/env-gitzebo.install.sh
+++ pkgspecs/env-gitzebo.install.sh
@@ -7,19 +7,27 @@ set -Eeo pipefail
 
 # Create the database.  Pre-create the file, so that the user can play with it.
 # This gets around patching Gitzebo directly for the moment.
-#gitzebo-schema create
 src_db="/usr/lib/python2.6/site-packages/gitzebo/gitzebo.db"
 tgt_db="/opt/db/gitzebo.db"
-#mkdir -p "$tgt_db"
-#mv "$src_db" "$tgt_db"
 ln -s "$tgt_db" "$src_db"
-#touch "$db"
-#chown git: "$db"
 
-#ln -s /usr/bin/gitzebo-dev-server /usr/bin/container.init
+# Create the directory to hold SSH information in.
+mkdir -pv /root/.ssh
+chmod 0700 /root/.ssh
+
+# Create links for persistent configuration files.
+# TODO: disabled due to hack below
+#ln -s {/opt/db,/root/.ssh}/authorized_keys
+ln -s {/opt/db,/etc/ssh}/ssh_host_key
+ln -s {/opt/db,/etc/ssh}/ssh_host_key.pub
+ln -s {/opt/db,/etc/ssh}/ssh_random_seed
+
 cat > /usr/bin/container.init <<'EOF'
 #!/bin/bash
 set -Eeo pipefail
+export HOME=/root
+source "$HOME/.bash_profile"
+
 repo_dir=/opt/git
 if [[ ! -e "$repo_dir" ]]
 then
@@ -39,23 +47,51 @@ then
 fi
 
 
+# TODO: HACK HACK HACK We need /root/.ssh/ to keep key data around.
+#       pkg.sh excludes /root from consideration for packaging, because
+#       it may contain transient data.  :[
+if [[ ! -d "/root/.ssh" ]]
+then
+  mkdir -pv /root/.ssh
+  chmod 0700 /root/.ssh
+fi
+if [[ ! -L "/root/.ssh/authorized_keys" ]]
+then
+  ln -s {/opt/db,/root/.ssh}/authorized_keys
+fi
+
+
 term_handler()
 {
   echo "Caught SIGTERM, shutting down"
   if [[ ! -z "$pid" ]]
   then
     echo "Sending SIGTERM to $pid"
-    kill -SIGTERM "$pid"
+    kill -SIGTERM "$web_pid"
+    /etc/init.d/sshd stop
   fi
 }
 trap term_handler 15
+
+gitzebo-regenerate-keyfile
+
+/usr/sbin/sshd -D -e &
+ssh_pid="$!"
+echo "SSH running on PID $ssh_pid"
+
 gitzebo-dev-server &
-pid="$!"
-echo "Server started with PID $pid"
+web_pid="$!"
+echo "Web server started with PID $web_pid"
+
+toret=0
 retval=0
-wait "$pid" || retval=$?
-echo "Server exited with return code $retval"
-exit "$retval"
+wait "$web_pid" || retval=$?
+echo "Web server exited with return code $retval"
+(( "$retval" )) && toret=$retval
+wait "$ssh_pid" || retval=$?
+echo "SSH server exited with return code $retval"
+(( "$retval" )) && toret=$retval
+exit "$toret"
 EOF
 chmod +x /usr/bin/container.init
 
