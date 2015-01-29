c2d547fc144de657d2accd4ba4591fdaa3a82439
John Gilik <john@jgilik.com>
2015-01-29 06:29:16 -0800

    Update to go 1.4.1, remove tests pending testing framework.

--- pkgspecs/go.make.sh
+++ pkgspecs/go.make.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #require ca-certificates wget
 pkg_name="go"
-pkg_version="1.4" # TODO: version crawler
+pkg_version="1.4.1" # TODO: version crawler
 echo "$pkg_version" > "/root/go.version"
 pkg_ext="tar.gz"
 pkg_url="https://storage.googleapis.com/golang/${pkg_name}${pkg_version}.src.${pkg_ext}"
