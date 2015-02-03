2f3e54c0ce7a1cf119d726a9bee52fb8e3e5e116
John Gilik <john@jgilik.com>
2015-02-02 17:47:05 -0800

    LFS i686 cross-compilation package: CLooG

---
+++ pkgspecs/i686-cross-cloog.deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -Eeo pipefail
+
+# Shared directory structure.
+echo i686-cross-root
+
+# Libraries we link against.
+echo i686-cross-gmp
+echo i686-cross-isl
---
+++ pkgspecs/i686-cross-cloog.make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -Eeo pipefail
+
+cd /root
+version=0.18.2
+echo "$version" > /root/version
+url="http://www.bastoul.net/cloog/pages/download/cloog-$version.tar.gz"
+wget "$url"
+
+tar -zxf "cloog-$version.tar.gz"
+cd cloog-*/
+
+export LDFLAGS="-Wl,-rpath,/cross-tools/i686/lib"
+./configure \
+  --prefix=/cross-tools/i686 \
+  --disable-static \
+  --with-gmp-prefix=/cross-tools/i686 \
+  --with-isl-prefix=/cross-tools/i686
+
+# "prevent the attempted installation of an invalid file" --CLFS
+# per http://www.clfs.org/view/CLFS-3.0.0-SYSVINIT/x86/cross-tools/cloog.html
+cp -v Makefile{,.orig}
+sed '/cmake/d' Makefile.orig > Makefile
+
+make
+
