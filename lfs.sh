41bdcaf2cf975a9415254f8ed6b5ce9d6e7e5834
John Gilik <john@jgilik.com>
2015-03-03 15:43:49 -0800

    Allow Linux builds to be resumed, and add references to all temp system packages

--- lfs.sh
+++ lfs.sh
@@ -3,12 +3,19 @@ set -Eeo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "This script builds all of Linux."
+start_from="$1"
+waiting=0
+if [[ ! -z "$start_from" ]]
+then
+  waiting=1
+fi
 
 pkgs=()
 pkgs+=("i686-clfs-root")
 pkgs+=("i686-cross-root")
 pkgs+=("i686-cross-env")
 pkgs+=("i686-tools-root")
+pkgs+=("i686-tools-env")
 for p in \
   file linux-headers m4 \
   ncurses pkg-config-lite gmp mpfr mpc isl cloog \
@@ -18,9 +25,27 @@ do
 done
 pkgs+=("i686-tools-glibc")
 pkgs+=("i686-cross-gcc")
+for p in \
+  gmp mpfr mpc isl cloog zlib binutils gcc ncurses bash bzip2 check coreutils \
+  diffutils file findutils gawk gettext grep gzip make patch sed tar texinfo \
+  util-linux xz
+do
+  pkgs+=("i686-tools-$p")
+done
+pkgs+=("i686-cross-bc")
 
 for p in "${pkgs[@]}"
 do
+  if (( "$waiting" ))
+  then
+    if [[ "$p" == "$start_from" ]]
+    then
+      waiting=0
+    else
+      echo "Ignoring package '$p'"
+      continue
+    fi
+  fi
   echo "Building package '$p'"
   retval=0
   "$DIR/pkg.from_name.sh" --pkg_name="$p" || retval=$?
