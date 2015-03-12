d5ffd409c1da15065e1821e53e410798f2190aa8
John Gilik <john@jgilik.com>
2015-03-12 10:57:41 -0700

    Disable manpages for nologin and groups binaries in shadow to prevent conflicts.
    util-linux already provides those binaries and manpages - but CLFS only disables
    the build of the binaries.

--- pkgspecs/i686-tools-shadow.make.sh
+++ pkgspecs/i686-tools-shadow.make.sh
@@ -17,10 +17,19 @@ cd shadow-*/
 #   other packages, and prevent Shadow from setting the suid bit on its
 #   installed programs:
 cp -v src/Makefile.in{,.orig}
-sed -e 's/groups$(EXEEXT) //' \
-    -e 's/= nologin$(EXEEXT)/= /' \
-    -e 's/\(^suidu*bins = \).*/\1/' \
-    src/Makefile.in.orig > src/Makefile.in
+sed \
+  -e 's/groups$(EXEEXT) //' \
+  -e 's/= nologin$(EXEEXT)/= /' \
+  -e 's/\(^suidu*bins = \).*/\1/' \
+  src/Makefile.in.orig > src/Makefile.in
+
+# The CLFS book failed to also excise the man pages from Shadow for the groups
+# and nologin programs, which causes packaging conflicts...
+cp -v man/Makefile.in{,.orig}
+sed -r \
+  -e 's@(\s+)\S*groups\.\S*(\s+)@\1\2@g' \
+  -e 's@(\s+)\S*nologin\.\S*(\s+)@\1\2@g' \
+  man/Makefile.in.orig > man/Makefile.in
 
 # Per the CLFS book:
 #   Tell Shadow to use passwd in /tools/i686/bin:
