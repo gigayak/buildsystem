648878a851ae6adf35e24cadd1f60ce3aaa96944
John Gilik <john@jgilik.com>
2015-03-25 23:36:55 -0700

    Remove dangling .opts.sh files, which were deprecated with tgz packaging.

--- go.opts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -Eeo pipefail
-
-pkg_name="$(echo "$PKG_NAME" | sed -re 's@^go-@@g')"
-
-echo -en "--provides\n$pkg_name\n"
--- pip.opts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeo pipefail
-
-pkg_name="$(echo "$PKG_NAME" | sed -re 's@^python-@@g')"
-
-pip show "$pkg_name" \
-  | grep 'Name:' \
-  | awk '{print $2}' \
-  | sed -re 's@^@--provides\n@g'
