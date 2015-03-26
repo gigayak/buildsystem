3cc06e7562c418cc6a05929d6197424c2c68383a
John Gilik <john@jgilik.com>
2015-03-25 22:09:45 -0700

    CLFS temporary system package: linux-devices

---
+++ pkgspecs/i686-tools-linux-devices.install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+# Per CLFS book:
+#   When the kernel boots the system, it requires the presence of a few device
+#   nodes, in particular the console and null devices. The device nodes will be
+#   created on the hard disk so that they are available before udev has been
+#   started, and additionally when Linux is started in single user mode (hence
+#   the restrictive permissions on console).
+mknod -m 0600 ${CLFS}/dev/console c 5 1
+mknod -m 0666 ${CLFS}/dev/null c 1 3
