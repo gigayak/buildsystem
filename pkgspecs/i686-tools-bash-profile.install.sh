6175324f1b322c8360bdaec0f2cf5aaaec2c2f2d
John Gilik <john@jgilik.com>
2015-03-27 10:45:16 -0700

    CLFS temporary system package: bash-profile

---
+++ pkgspecs/i686-tools-bash-profile.install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+# Per CLFS book:
+#   The new instance of the shell that will start when the system is booted is a
+#   login shell, which will read the .bash_profile file. Create .bash_profile
+#   now:
+cat > ${CLFS}/root/.bash_profile << "EOF"
+set +h
+PS1='\u:\w\$ '
+LC_ALL=POSIX
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin:/tools/sbin
+export LC_ALL PATH PS1
+EOF
