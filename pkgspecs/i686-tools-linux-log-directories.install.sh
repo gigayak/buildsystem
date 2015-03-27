cf278e4c6c72176d588c36899a42dd63e68d2c5d
John Gilik <john@jgilik.com>
2015-03-27 10:45:15 -0700

    CLFS temporary system package: linux-log-directories

---
+++ pkgspecs/i686-tools-linux-log-directories.install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+# Per the CLFS book:
+#   The login, agetty, and init programs (and others) use a number of log files
+#   to record information such as who was logged into the system and when.
+#   However, these programs will not write to the log files if they do not
+#   already exist. Initialize the log files and give them proper permissions:
+touch ${CLFS}/var/log/{btmp,lastlog,wtmp}
+chgrp -v 13 ${CLFS}/var/log/lastlog
+chmod -v 664 ${CLFS}/var/log/lastlog
+chmod -v 600 ${CLFS}/var/log/btmp
