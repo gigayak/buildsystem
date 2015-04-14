99eda3bcfe61306e617ee192fa7e1269ab754156
John Gilik <john@jgilik.com>
2015-04-13 17:28:02 -0700

    Convert CLFS patch to automated fix.

--- pkgspecs/i686-tools-sysvinit.make.sh
+++ pkgspecs/i686-tools-sysvinit.make.sh
@@ -16,7 +16,26 @@ cd sysvinit-*/
 # Per CLFS book:
 #   Apply a patch to prevent installation of unneeded programs, and allow
 #   sysvinit to be installed in /tools/i686:
-patch -Np1 -i "../sysvinit-$version-$patch"
+#patch -Np1 -i "../sysvinit-$version-$patch"
+cp -v src/Makefile{,.orig}
+# TODO: The target-based sed deletions could be /.../d expressions.
+sed -r \
+  -e 's@^\s*last[o.]*:.*$@@g' \
+  -e 's@^(\s*)ln -sf last.*$@\1echo nop >/dev/null; \\@g' \
+  -e 's@last\S*@@g' \
+  -e 's@^\s*mesg[o.]*:.*$@@g' \
+  -e 's@mesg\S*@@g' \
+  -e 's@^\s*sulogin[o.]*:.*$@@g' \
+  -e 's@sulogin\S*@@g' \
+  -e 's@^\s*utmpdump[o.]*:.*$@@g' \
+  -e 's@utmpdump\S*@@g' \
+  -e 's@^\s*mountpoint[o.]*:.*$@@g' \
+  -e 's@mountpoint\S*@@g' \
+  src/Makefile.orig > src/Makefile
+cp -v src/paths.h{,.orig}
+sed -r \
+  -e 's@^(\#define\s+INITTAB\s+")[^"]+(".*$)@\1/tools/i686/etc/inittab\2@g' \
+  src/paths.h.orig > src/paths.h
 
 # Non-standard make process...
 make -C src clobber
