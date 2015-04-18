aceeee1f19dcc0d18483dcfd8c3a30e28084cc61
John Gilik <john@jgilik.com>
2015-04-17 17:37:22 -0700

    Ensure /proc is mounted upon boot - it's needed by dhclient.

--- pkgspecs/i686-tools-linux-fstab.install.sh
+++ pkgspecs/i686-tools-linux-fstab.install.sh
@@ -9,10 +9,15 @@ source /tools/env.sh
 cat > ${CLFS}/etc/fstab << "EOF"
 # file system  mount-point  type   options          dump  fsck
 #                                                         order
-
 /dev/sda1      /            ext2   defaults         1     1
 /dev/sda2      swap         swap   pri=1            0     0
 devpts         /dev/pts     devpts gid=5,mode=620   0     0
 shm            /dev/shm     tmpfs  defaults         0     0
+none           /proc        proc   defaults         0     0
 EOF
-
+# PS: I added /proc myself so that dhclient would work.  Without proc, it barfs:
+#   Error opening '/proc/net/dev' to list interfaces
+#   Can't get list of interfaces.
+# Instructions were from:
+#   http://www.microhowto.info/troubleshooting/mounting_proc.html
+# TODO: Is the first token in the /proc mount ("none") okay?
