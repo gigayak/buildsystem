6289588bcf37446577339a5df55e5da2cc057fcb
John Gilik <john@jgilik.com>
2015-04-21 17:10:05 -0700

    Remove swap disk for simplicity.
    We can always add it back in if we need it, but that will require creating an
    image for it, and keeping track of the image.

--- pkgspecs/i686-tools-linux-fstab.install.sh
+++ pkgspecs/i686-tools-linux-fstab.install.sh
@@ -10,7 +10,6 @@ cat > ${CLFS}/etc/fstab << "EOF"
 # file system  mount-point  type   options          dump  fsck
 #                                                         order
 /dev/sda1      /            ext2   defaults         1     1
-/dev/sda2      swap         swap   pri=1            0     0
 devpts         /dev/pts     devpts gid=5,mode=620   0     0
 shm            /dev/shm     tmpfs  defaults         0     0
 none           /proc        proc   defaults         0     0
