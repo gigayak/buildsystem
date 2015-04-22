9f25ba46d155e81cab5fe148c0b10949e422ac56
John Gilik <john@jgilik.com>
2015-04-21 17:10:53 -0700

    Add /bin/sh symlink to shut up init scripts which rely on it.

--- pkgspecs/i686-tools-bash-aliases.install.sh
+++ pkgspecs/i686-tools-bash-aliases.install.sh
@@ -5,3 +5,5 @@ source /tools/env.sh
 # Per CLFS book:
 #   Many bash scripts specify /bin/bash.
 ln -sv /tools/i686/bin/bash ${CLFS}/bin
+# The init scripts specify /bin/sh.
+ln -sv /tools/i686/bin/bash ${CLFS}/bin/sh
