c7bcb627cde902152a02b7e52d6a07fd13d0320a
John Gilik <john@jgilik.com>
2015-05-08 20:49:38 -0700

    Package spec for dnsmasq.

---
+++ pkgspecs/dnsmasq.builddeps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -Eeo pipefail
+
+# to get source
+echo tar # to extract source
+echo wget # to download source
+
+# to build source
+echo gcc
+echo automake
+echo autoconf
---
+++ pkgspecs/dnsmasq.make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -Eeo pipefail
+
+version=2.72
+echo "$version" > /root/version
+url="http://www.thekelleys.org.uk/dnsmasq/dnsmasq-$version.tar.gz"
+
+cd /root
+wget "$url"
+tar -zxf *.tar.gz
+cd */
+
+make PREFIX=/usr
+
