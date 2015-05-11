f51cfa51e292c06e41e41bb5d59a3bcb4484860d
John Gilik <john@jgilik.com>
2015-05-11 16:58:57 -0700

    CLFS temporary system package: nettle
    Needed to compile gnutls, which is needed for SSL support in wget.

---
+++ pkgspecs/i686-tools-nettle.make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+#version=3.1.1
+version=2.7.1 # gnutls does not support 3.0 and up
+echo "$version" > /root/version
+url="https://ftp.gnu.org/gnu/nettle/nettle-$version.tar.gz"
+
+cd /root
+wget "$url"
+tar -zxf *.tar.gz
+cd */
+
+./configure \
+  --prefix=/tools/i686 \
+  --build="${CLFS_HOST}" \
+  --host="${CLFS_TARGET}"
+make
+
+# Do not allow info files to be installed.
+#
+# /usr/share/info/dir is a single file to hold an index, and is incompatible
+# with file-based package management.
+#rm -rf /usr/share/info
