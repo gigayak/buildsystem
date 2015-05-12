fae4019cab915b6309751ccfd9a1960646e30509
John Gilik <john@jgilik.com>
2015-05-11 17:01:32 -0700

    Add SSL support to cross-compiled wget.

--- pkgspecs/i686-tools-wget.make.sh
+++ pkgspecs/i686-tools-wget.make.sh
@@ -11,13 +11,8 @@ tar -zxf *.tar.gz
 
 cd *-*/
 
-# --without-ssl removes HTTPS support, but with the benefit of not requiring us
-# to attempt cross compilation of GNUTLS and Nettle before hand.
-#
-# TODO: We may want to use HTTPS for serving source packages in the future :(
 ./configure \
   --prefix=/tools/i686 \
   --build="${CLFS_HOST}" \
-  --host="${CLFS_TARGET}" \
-  --without-ssl
+  --host="${CLFS_TARGET}"
 make
--- pkgspecs/i686-tools.deps.sh
+++ pkgspecs/i686-tools.deps.sh
@@ -10,7 +10,8 @@ for p in \
   gcc-aliases bash-aliases coreutils-aliases grep-aliases \
   file-aliases sysvinit-aliases shadow-aliases linux-aliases linux-devices \
   linux-credentials linux-fstab linux-log-directories bash-profile \
-  iproute2 dhcp dhcp-config dropbear dropbear-config wget rsync buildsystem
+  iproute2 dhcp dhcp-config dropbear dropbear-config nettle gnutls wget rsync \
+  buildsystem
 do
   if [[ "$PKG_NAME" == "i686-tools-$p" ]]
   then
