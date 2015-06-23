a5b324d2c6182d79ced1d57ac04135fa6181e9fd
John Gilik <john@jgilik.com>
2015-06-23 08:53:49 -0700

    New package mountpoints allows CD to boot by prepopulating mountpoints.
    Without this, it would mount the rootfs read-only and fail to create the
    mountpoints when attaching filesystems to the hierarchy.  Oops!

---
+++ pkgspecs/i686-tools-linux-mountpoints.install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+# This initializes the mountpoints that are needed to boot.  This allows us
+# to boot even with the rootfs mounted read-only - such as in the install
+# CD use case (or recovery attempt use case).
+mkdir -pv "$CLFS"/{proc,sys,run{,/lock},tmp}
+
+# We likely have /proc and /sys mounted - so let's explicitly tell the
+# packager to pick them up.
+cat >> /root/extra_installed_paths <<EOF
+$CLFS/proc
+$CLFS/sys
+$CLFS/tmp
+EOF
---
+++ pkgspecs/i686-tools-linux-mountpoints.version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -Eeo pipefail
+
+echo 1.0
