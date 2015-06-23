44c25797c00995d999d537b5545df9d3a51e8e98
John Gilik <john@jgilik.com>
2015-06-23 08:51:47 -0700

    Split fstab for hard disk and CD installs.

---
+++ pkgspecs/i686-tools-linux-fstab-cd.install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -Eeo pipefail
+source /tools/env.sh
+
+# Per CLFS book:
+#   The /etc/fstab file is used by some programs to determine where file systems
+#   are to be mounted by default, which must be checked, and in which order.
+#   Create a new file systems table like this:
+cat > ${CLFS}/etc/fstab << "EOF"
+# file system  mount-point  type   options          dump  fsck
+#                                                         order
+/dev/sr0       /            udf,iso9660 defaults    0     0
+devpts         /dev/pts     devpts gid=5,mode=620   0     0
+shm            /dev/shm     tmpfs  defaults         0     0
+tmpfs          /tmp         tmpfs  defaults         0     0
+none           /proc        proc   defaults         0     0
+EOF
+# PS: I added /proc myself so that dhclient would work.  Without proc, it barfs:
+#   Error opening '/proc/net/dev' to list interfaces
+#   Can't get list of interfaces.
+# Instructions were from:
+#   http://www.microhowto.info/troubleshooting/mounting_proc.html
+# TODO: Is the first token in the /proc mount ("none") okay?
