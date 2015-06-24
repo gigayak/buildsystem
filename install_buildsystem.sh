e980d79442128ea4df6df5e63dd10584a80e6b25
John Gilik <john@jgilik.com>
2015-06-23 17:12:30 -0700

    Make it easy to copy the locally-checked-out copy of the buildsystem around.
    This makes it easier to use a modified buildsystem in chroots, as building
    the package requires changes to be committed and pushed.

---
+++ install_buildsystem.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -Eeo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/flag.sh"
+
+add_flag --required output_path "Where to store the build system"
+parse_flags
+
+if [[ ! -e "$F_output_path" ]]
+then
+  echo "$(basename "$0"): cannot find output path $(sq "$F_output_path")" >&2
+  echo "$(basename "$0"): this script doesn't want to flood an unknown path" >&2
+  echo "$(basename "$0"): run: mkdir $(sq "$F_output_path")" >&2
+  exit 1
+fi
+
+retval=0
+rsync \
+  --archive \
+  --exclude="cache/*" \
+  --exclude=".git" \
+  "$DIR/" \
+  "$F_output_path/" \
+|| retval=$?
+if (( "$retval" ))
+then
+  echo "$(basename "$0"): rsync failed with return code $retval" >&2
+  exit 1
+fi
+
+exit 0
