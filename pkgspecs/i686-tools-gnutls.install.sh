723457c4d257718e69795d4d2a1755ae629ef092
John Gilik <john@jgilik.com>
2015-06-24 18:25:49 -0700

    Create SSL configuration directory, so other packages can populate it.

--- pkgspecs/i686-tools-gnutls.install.sh
+++ pkgspecs/i686-tools-gnutls.install.sh
@@ -8,3 +8,7 @@ make install
 
 # See make.sh for the dirty details on this line.
 rm -rf /tools/i686/share/info
+
+# UGH - make install should really create all necessary config directories.
+# But... it doesn't.
+mkdir -pv /tools/i686/etc/ssl/certs
