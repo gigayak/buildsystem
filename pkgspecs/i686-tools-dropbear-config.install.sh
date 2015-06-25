5c9357fbb4b4f320576412f7b59854759dcfd0ed
John Gilik <john@jgilik.com>
2015-06-24 18:22:17 -0700

    Allow dropbear to try multiple configuration directories; necessary for CDs.
    CDs are read-only media, and we need a writable configuration directory on
    first boot to avoid using static host keys shared by all hosts.  (Security
    reasons make that a really bad idea!)  To make this possible, we can use
    /tmp when the usual spot isn't writable.

--- pkgspecs/i686-tools-dropbear-config.install.sh
+++ pkgspecs/i686-tools-dropbear-config.install.sh
@@ -49,12 +49,69 @@ ln -sv ../init.d/eth0 "$CLFS/tools/i686/etc/rc.d/rcsysinit.d/S70eth0"
 cat > "$CLFS/tools/i686/etc/rc.d/init.d/dropbear" <<'EOF'
 #!/bin/bash
 set -Eeo pipefail
-rsa_key=/tools/i686/etc/dropbear/rsa_host_key
+
+# List of directories to attempt finding keys in.
+#
+# If no keys are found, then it's the list of paths to attempt
+# writing keys to.
+#
+# Paths are evaluated in the order they're added to the array.
+#
+# HACK SCALE: PUNY
+#
+# This feature exists solely to allow Dropbear to work when
+# booting from a live CD, where /tools/i686/etc/dropbear is not
+# writable.  We could avoid this if we used a union filesystem
+# which allows COW-to-memory writes to the read-only image, but
+# this would require kernel changes.  This hack works with a
+# stock kernel juuuust fine.
+key_dirs=()
+key_dirs+=("/tools/i686/etc/dropbear")
+key_dirs+=("/tmp")
+
+keys=(rsa_host_key)
+
+found=0
+for key_dir in "${key_dirs[@]}"
+do
+  for key in "${keys[@]}"
+  do
+    if [[ -e "$key_dir/$key" ]]
+    then
+      found=1
+      break
+    fi
+  done
+done
+if (( ! "$found" ))
+then
+  for key_dir in "${key_dirs[@]}"
+  do
+    if [[ -w "$key_dir" ]]
+    then
+      found=1
+      break
+    fi
+  done
+fi
+if (( ! "$found" ))
+then
+  echo "Unable to find viable directory to store host keys in." >&2
+  exit 1
+fi
+
+rsa_key="$key_dir/rsa_host_key"
 if [[ ! -e "$rsa_key" ]]
 then
   dropbearkey -t rsa -s 1024 -f "$rsa_key"
 fi
-dropbear -B -K 15 -I 120 -r "$rsa_key"
+# -B: allow blank password to login (say, for root)
+# -K <arg>: keepalive every <arg> seconds
+# -I <arg>: disconnect clients after <arg> seconds of idle time
+#   (0 disables both -K and -I)
+# -r <arg>: read RSA host key from <arg>
+# -E: log to stderr instead of syslog
+dropbear -B -K 60 -I 3600 -r "$rsa_key" -E
 EOF
 chmod +x "$CLFS/tools/i686/etc/rc.d/init.d/dropbear"
 ln -sv ../init.d/dropbear "$CLFS/tools/i686/etc/rc.d/rc3.d/S20dropbear"
