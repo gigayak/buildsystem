35d26e97c5067d3f092ab63cd558aed844b807a9
John Gilik <john@jgilik.com>
2015-07-23 08:07:54 -0700

    Add the /boot directory to /tools/i686.
    Bootloaders will be looking here for the kernel.

--- pkgspecs/i686-tools-root.install.sh
+++ pkgspecs/i686-tools-root.install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -Eeo pipefail
-install -dv /clfs-root/tools/i686/{lib,bin,include,share/info/dir}
+install -dv /clfs-root/tools/i686/{lib,bin,include,share/info/dir,boot}
 ln -sv /clfs-root/tools /
