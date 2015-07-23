35b163bd81330cd99c41092d0c946191100042f1
John Gilik <john@jgilik.com>
2015-07-23 08:04:15 -0700

    Automate generation of SSL certificates.

--- create_crypto.sh
+++ create_crypto.sh
@@ -2,96 +2,142 @@
 set -Eeo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source "$DIR/cleanup.sh"
 source "$DIR/escape.sh"
 
-echo "Regenerating all missing crypto files."
-echo "These should never be checked in."
-echo "However, setting up the trust for new keys can be a pain..."
+echo "$(basename "$0"): Regenerating all missing crypto files."
+echo "$(basename "$0"): These should never be checked in."
 
+# TODO: refactor this into multiple scripts
+# key_strength applies to SSH keys
 key_strength="8192"
 rsa_key_strength="$key_strength"
 dsa_key_strength="$key_strength"
 ssl_key_strength="$key_strength"
 #storage_dir="/root/crypto"
 
-types=(rsa dsa ssl)
-rsa_keys=()
-dsa_keys=()
-ssl_keys=()
-
-add_key()
+ssh_key()
 {
-  if (( "$#" < 1 ))
+  algo="$1"
+  key_name="$2"
+  if [[ -z "$algo" || -z "$key_name" ]]
   then
-    echo "Usage: ${FUNCNAME[0]} <key_user_name> <type> [<type> ...]" >&2
-    echo >&2
-    echo "<type> can be one of:" >&2
-    echo "  RSA: SSH RSA key." >&2
-    echo "  DSA: SSH DSA key." >&2
-    echo "  SSL: CA-signed SSL key." >&2
+    echo "Usage: ${FUNCNAME[0]} <algorithm> <key name>" >&2
     return 1
   fi
 
-  key_name="$1"
-  shift
-
-  for type in "$@"
-  do
-    found=0
-    for allowed_type in "${types[@]}"
-    do
-      if [[ "$allowed_type" == "$type" ]]
-      then
-        found=1
-        break
-      fi
-    done
-    if (( ! "$found" ))
-    then
-      echo "${FUNCNAME[0]}: unknown crypto key type '$type'" >&2
-      return 1
-    fi
-    type_array="${type}_keys"
-    eval "$type_array+=($(sq "$key_name"))"
-  done
-}
-
-add_key godev rsa
-add_key gitzebo rsa dsa
-
-for algo in rsa dsa
-do
-  echo "Creating all SSH $algo keys."
-  array_name="${algo}_keys[@]"
   algo_name="$(echo "$algo" | tr '[:lower:]' '[:upper:]')"
   strength_name="${algo}_key_strength"
   key_strength="${!strength_name}"
-  for key_name in "${!array_name}"
-  do
-    #key_path="$storage_dir/${key_name}.ssh.${algo}"
-    key_path="$("$DIR/get_crypto.sh" \
-      --path_only \
-      --private \
-      --key_type="$algo" \
-      --key_name="$key_name")"
-    if [[ -e "$key_path" ]]
-    then
-      echo "$algo_name SSH key for ${key_name} already exists."
-      continue
-    fi
-    echo "Generating $key_strength-bit $algo_name SSH key for '${key_name}'..."
-    echo "8192 bit keys take ~10 minutes."
-    time ssh-keygen \
-      -t "$algo" \
-      -C "john@jgilik.com" \
-      -b "$key_strength" \
-      -f "$key_path" \
-      -N ''
-  done
-done
+  key_path="$("$DIR/get_crypto.sh" \
+    --path_only \
+    --private \
+    --key_type="$algo" \
+    --key_name="$key_name")"
+  if [[ -e "$key_path" ]]
+  then
+    echo "${FUNCNAME[0]}: $algo_name SSH key for ${key_name} exists" >&2
+    return 0
+  fi
+  echo "${FUNCNAME[0]}: generating $key_strength-bit $algo_name" \
+    "SSH key for '${key_name}'" >&2
+  echo "${FUNCNAME[0]}: 8192 bit keys take ~10 minutes." >&2
+  time ssh-keygen \
+    -t "$algo" \
+    -C "john@jgilik.com" \
+    -b "$key_strength" \
+    -f "$key_path" \
+    -N ''
+}
+ssh_rsa_key()
+{
+  ssh_key rsa "$@"
+}
+ssh_dsa_key()
+{
+  ssh_key dsa "$@"
+}
 
-for key_name in "${ssl_keys[@]}"
+# create a named temp root from _TEMP_ROOTS
+# check for necessary bind mounts to CA configuration
+# chroot and create keys
+# move keys out
+temp_root=""
+for root in "${_TEMP_ROOTS[@]}"
 do
-  echo "$(basename "$0"): asked for SSL key for '$key_name' - not impl." >&2
-  exit 1
+  if [[ -e "$root" ]]
+  then
+    temp_root="$root"
+    break
+  fi
 done
+if [[ -z "$temp_root" ]]
+then
+  echo "$(basename "$0"): unable to find writable temp root" >&2
+  exit 1
+fi
+root="$temp_root/chroot.certificate_authority"
+if [[ ! -e "$root" ]]
+then
+  mkdir -pv "$root"{,/proc,/sys,/opt/ca}
+  ./install_pkg.sh \
+    --install_root="$root" \
+    --pkg_name="env-ca"
+fi
+for mnt in /proc /sys /dev
+do
+  if ! mountpoint -q -- "$root$mnt"
+  then
+    mount --bind "$mnt" "$root$mnt"
+  fi
+done
+ca_root="$root/opt/ca"
+if ! mountpoint -q -- "$ca_root"
+then
+  mount --bind /root/localstorage/certificate-authority/ca "$ca_root"
+fi
+
+ssl_cert()
+{
+  key_name="$1"
+  shift
+  if [[ -z "$key_name" ]]
+  then
+    echo "Usage: ${FUNCNAME[0]} <key_name> [extra domain name] ..." >&2
+    return 1
+  fi
+  key_path="$ca_root/keys/$key_name.key"
+  crt_path="$ca_root/certificates/$key_name.crt"
+  if [[ -e "$key_path" && -e "$crt_path" ]]
+  then
+    echo "${FUNCNAME[0]}: found SSL cert $(sq "$key_name"), skipping" >&2
+    return 0
+  fi
+  echo "${FUNCNAME[0]}: creating SSL certs for $(sq "$key_name")" >&2
+  for san in "$@"
+  do
+    echo "${FUNCNAME[0]}: adding alternate name $(sq "$san")" >&2
+  done
+  chroot "$root" ca_generate_certificate "$key_name" "$@"
+  tgt_path="/root/localstorage/$key_name/ssl"
+  if [[ ! -e "$tgt_path" ]]
+  then
+    mkdir -pv "$tgt_path"
+  fi
+  cp -vf \
+    "$ca_root/keys/$key_name.key" \
+    "$tgt_path/$key_name.key"
+  cp -vf \
+    "$ca_root/certificates/$key_name.crt" \
+    "$tgt_path/$key_name.crt"
+}
+
+
+ssh_rsa_key godev
+ssh_rsa_key gitzebo
+ssh_dsa_key gitzebo
+ssl_cert gitzebo
+ssl_cert proxy '*.jgilik.com'
+ssl_cert repo
+
+echo "$(basename "$0"): successfully created all keys and certs" >&2
