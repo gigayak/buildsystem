f0a6b01168ce5b8db401edddb4cba206bf2995c5
John Gilik <john@jgilik.com>
2015-08-12 20:13:44 -0700

    Propagate linux-headers cross -> tools switch to glibc.

--- pkgspecs/i686-tools-glibc.builddeps.sh
+++ pkgspecs/i686-tools-glibc.builddeps.sh
@@ -8,6 +8,6 @@ echo tar
 # To build.
 echo i686-cross-env
 echo i686-cross-gcc-static
-echo i686-cross-linux-headers
+
 # Okay, really, it's the last time... I hope. BUILD_CC=gcc; so we need gcc...
 echo gcc
