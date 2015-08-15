5730fe4b10c5b40d48aaea3db14aa4bb3f7da14d
John Gilik <john@jgilik.com>
2015-08-15 15:37:51 -0700

    Open ports for webservers to allow proxy to see outside world.

--- create_network.sh
+++ create_network.sh
@@ -94,6 +94,8 @@ cat >> "$rules" <<EOF
 -A INPUT -p icmp -j ACCEPT 
 -A INPUT -i lo -j ACCEPT 
 -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT 
+-A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT 
+-A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT 
 -A INPUT -j REJECT --reject-with icmp-host-prohibited 
 COMMIT
 EOF
