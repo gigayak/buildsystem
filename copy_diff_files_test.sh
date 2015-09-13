c69a0930f3c336e71ebd89c0702d2556beeda246
John Gilik <john@jgilik.com>
2015-09-12 17:26:26 -0700

    Add unit tests for copy_diff_files.py.
    The stage2 image does not have a Python interpreter.  The Python
    interpreter is annoying to cross-compile.  The stage2 image does
    have a bash interpreter, though.  Since the stage2 image needs to
    be capable of building new packages this is a problem, so...
    
    copy_diff_files.py is about to be rewritten in bash.  These tests
    will make it easier to have some vague reassurances that the
    replacement will work.

---
+++ copy_diff_files_test.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -Eeo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/cleanup.sh"
+
+start_case()
+{
+  export case_name="$@"
+}
+pass()
+{
+  echo "PASS: $case_name"
+}
+fail()
+{
+  echo "FAIL: $case_name"
+}
+
+ddiff()
+{
+  if (( "$#" != 2 ))
+  then
+    echo "Usage: ${FUNCNAME[0]} <post-install snapshot> <pre-install>" >&2
+    return 1
+  fi
+  rsync \
+    --dry-run --recursive --itemize-changes \
+    --links --perms --group --owner \
+    --devices --specials \
+    "$1/" "$2/"
+}
+
+export copy_diff_files="$DIR/copy_diff_files.py"
+
+(
+  make_temp_dir pre
+  make_temp_dir post
+  make_temp_dir pkg
+  mkdir -pv "$post/etc/silliness"
+  ddiff "$post" "$pre" | "$copy_diff_files" "$post" "$pkg"
+  start_case "basic directory creation"
+  [[ -d "$pkg/etc/silliness" ]] && pass || fail
+)
+
+(
+  make_temp_dir pre
+  make_temp_dir post
+  make_temp_dir pkg
+  touch "$post/test_file"
+  ddiff "$post" "$pre" | "$copy_diff_files" "$post" "$pkg"
+  start_case "basic file creation"
+  [[ -f "$pkg/test_file" ]] && pass || fail
+)
+
+(
+  make_temp_dir pre
+  make_temp_dir post
+  make_temp_dir pkg
+  mknod "$post/null" c 1 3
+  chmod 666 "$post/null"
+  ddiff "$post" "$pre" | "$copy_diff_files" "$post" "$pkg"
+  start_case "basic character device creation existence check"
+  [[ -c "$pkg/null" ]] && pass || fail
+  start_case "basic character device major/minor check"
+  [[ "$(stat -c '%t,%T' "$pkg/null")" == "1,3" ]] && pass || fail
+  start_case "basic character device creation permissions check"
+  [[ "$(stat -c '%a' "$pkg/null")" == "666" ]] && pass || fail
+)
+
+(
+  make_temp_dir pre
+  make_temp_dir post
+  make_temp_dir pkg
+  ln -sv "/path/does/not/exist" "$post/symlink"
+  ddiff "$post" "$pre" | "$copy_diff_files" "$post" "$pkg"
+  start_case "basic symlink creation existence check"
+  [[ -L "$pkg/symlink" ]] && pass || fail
+  start_case "basic symlink creation target check"
+  [[ "$(readlink "$pkg/symlink")" == "/path/does/not/exist" ]] \
+    && pass || fail
+)
