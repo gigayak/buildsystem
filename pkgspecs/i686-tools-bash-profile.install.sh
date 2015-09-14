a454d83089bfbd492192466165c369ca33d706d6
John Gilik <john@jgilik.com>
2015-09-14 08:12:08 -0700

    Add TODO to make .bash_profile more explicit in stage3.

--- pkgspecs/i686-tools-bash-profile.install.sh
+++ pkgspecs/i686-tools-bash-profile.install.sh
@@ -6,6 +6,10 @@ source /tools/env.sh
 #   The new instance of the shell that will start when the system is booted is a
 #   login shell, which will read the .bash_profile file. Create .bash_profile
 #   now:
+# TODO: Make this explicit in the stage3 build context: right now, this profile
+#   is inherited implicitly in stage3 build scripts, which makes them a bit
+#   more difficult to read (as it's unclear that /tools/i686/bin is in the
+#   PATH).
 cat > ${CLFS}/root/.bash_profile << "EOF"
 set +h
 PS1='\u:\w\$ '
