e50aae8070a7c50cc5f0d6dab83b419c1adde088
John Gilik <john@jgilik.com>
2015-09-15 09:13:17 -0700

    Script installation of buildsystem on remote stage2 server.
    I've been doing this a lot manually from the commandline when
    developing; may as well rely on git instead of bash history.

---
+++ lfs.stage3.install_buildsystem.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -Eeo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/cleanup.sh"
+
+ip="$1"
+if [[ -z "$ip" ]]
+then
+  echo "Usage: $(basename "$0") <stage2 server IP>" >&2
+  exit 1
+fi
+
+make_temp_dir temp
+
+"$DIR/install_buildsystem.sh" \
+  --output_path="$temp"
+tar -c -C "$temp" . \
+  | ssh \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    -o KeepAlive=yes \
+    -o ServerAliveInterval=15 \
+    "root@$ip" \
+    '/tools/i686/bin/rm -rf /tools/i686/bin/buildsystem \
+      && /tools/i686/bin/mkdir -pv /tools/i686/bin/buildsystem \
+      && /tools/i686/bin/tar -x -C /tools/i686/bin/buildsystem'
