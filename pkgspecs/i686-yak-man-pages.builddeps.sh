37911ff59fde3cbe4b2a4fdaaa4036560324d776
John Gilik <john@jgilik.com>
2015-09-14 17:20:01 -0700

    Initial commit of kernel man pages.

---
+++ pkgspecs/i686-yak-man-pages.builddeps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -Eeo pipefail
+
+echo i686-tools2-wget
+echo i686-tools2-tar
+echo i686-tools2-make
---
+++ pkgspecs/i686-yak-man-pages.make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -Eeo pipefail
+
+cd /root
+version="4.02"
+echo "$version" > /root/version
+base_url="https://www.kernel.org/pub/linux/docs/man-pages"
+current_url="$base_url/man-pages-$version.tar.gz"
+archive_url="$base_url/Archive/man-pages-$version.tar.gz"
+# TODO: Figure out how to get HTTPS CA certs installed so we don't
+#   have to ignore certificate checks here.
+if ! wget --no-check-certificate "$current_url" \
+  && ! wget --no-check-certificate "$archive_url"
+then
+  echo "Failed to retrieve man-pages v$version." >&2
+  echo "Tried:" >&2
+  echo " - $current_url" >&2
+  echo " - $archive_url" >&2
+  exit 1
+fi
+
+tar -zxf *.tar.gz
