e37593afd87fa451b006b320aa5b0cd5cc037bbf
John Gilik <john@jgilik.com>
2015-11-24 15:06:15 -0800

    Allow chroots to see all package sources, and to use correct resolv.
    Ubuntu chroots were being created with an apt configuration that
    contained a minimal number of apt repositories.  Prior to this change,
    any attempts to fetch apt packages from the "universe" repository
    in a chroot would fail.
    
    Additionally, networking was breaking when /etc/resolv.conf from
    create_resolv.sh was required, as this custom resolv.conf was being
    overwritten by the base-ubuntu package.  base-ubuntu no longer
    writes out an /etc/resolv.conf.

--- pkgspecs/base-ubuntu.bootstrap.sh
+++ pkgspecs/base-ubuntu.bootstrap.sh
@@ -26,11 +26,31 @@ chroot "$root" dpkg --get-selections \
 
 cd "$root"
 
+# Copy in host apt configuration, as the default debootstrap apt configuration
+# is pretty minimal compared to the default installed apt configuration.
+#
+# Copying the host configuration in might break across different hosts, if
+# there's a lot of variance in configurations across hosts.
+#
+# Having a static configuration here might break across different versions, as
+# it might not be forwards/backwards compatible.
+#
+# Since breakages due to forwards/backwards compatibility would require more
+# code to fix, and I don't think the host configuration varies much outside
+# of third-party proprietary stuff (Flash, codecs, etc), copying in the host
+# configuration was chosen.
+cp "/etc/apt/sources.list" "$root/etc/apt/sources.list"
+chroot "$root" apt-get update >&2
+
 # Make sure /dev is empty.  Without this passage, it gets littered with
 # block devices and causes copy_diff_files.sh to attempt a bunch of device
 # creations, leading to it crashing.  Instead, we want to use bind-mounts.
 rm -rf dev
 mkdir dev
 chmod 1755 dev
+
+# Make sure /etc/resolv.conf is empty.  It would overwrite the resolv.conf
+# installed by the buildsystem when this package is installed.
+rm -f etc/resolv.conf
 
 tar -cz -C "$root" .
