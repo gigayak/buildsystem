13034d764589cb08ba36a597bee6e7287f9682ad
John Gilik <john@jgilik.com>
2015-11-26 17:39:20 -0800

    Prevent GMP builds from failing when cross compiling Linux.
    ABI=32 on a 64-bit host caused messages along the lines of
    "error: size of array 'test_array' is negative" and "configure:
    error: could not find a working compiler, see config.log for
    details".
    
    Looking at the CLFS book, it appears ABI=32 does not exist
    in the latest development version, and removing it from the
    commandline clears the errors.

--- pkgspecs/i686-cross-gmp.make.sh
+++ pkgspecs/i686-cross-gmp.make.sh
@@ -9,11 +9,8 @@ url="http://ftp.gnu.org/gnu/gmp/gmp-$version.tar.xz"
 wget "$url"
 tar -Jxf "gmp-$version.tar.xz"
 
-# Hack to avoid gmp-6.0.0a.tar.gz expanding to gmp-6.0.0
-# TODO: Remove this hack with common prefix removal for tarball extraction.
 cd gmp-*/
-arch_bits=32 # Set to 64 to build for x86_64
-./configure ABI="$arch_bits" \
+./configure \
   --prefix=/cross-tools/i686 \
   --enable-cxx \
   --disable-static
