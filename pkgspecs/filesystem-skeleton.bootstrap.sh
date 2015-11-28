7cc8948e2bb5fb24b816706ede0f984c1b2a03b1
John Gilik <john@jgilik.com>
2015-11-27 18:02:49 -0800

    Make filesystem-skeleton find cleanup.sh by removing hack.
    Accidentally decided that putting DIR="script directory/.." made
    sense in the filesystem-skeleton bootstrap script.  Then, saw the
    fact that $DIR/cleanup.sh was being included and changed it to be
    $DIR/../cleanup.sh... which became the same as $DIR/../../cleanup.sh
    for other scripts' usages of $DIR due to the inconsistent setting
    of $DIR.  cleanup.sh was not found when this was the case...

--- pkgspecs/filesystem-skeleton.bootstrap.sh
+++ pkgspecs/filesystem-skeleton.bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -Eeo pipefail
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/../cleanup.sh"
 
 make_temp_dir root
