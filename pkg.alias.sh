50d0d1e11baf7f3a0d160fc93ce67bdce12ec55a
John Gilik <john@jgilik.com>
2015-11-30 18:02:09 -0800

    Add a script to alias one package to another.
    I forgot to add this to the commit before last.  I should learn
    rebase.

---
+++ pkg.alias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -Eeo pipefail
+DIR(){(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+
+source "$(DIR)/repo.sh"
+source "$(DIR)/flag.sh"
+add_flag --required alias "Name of the virtual package to create."
+add_flag --required target "Name of actual package to be installed"
+parse_flags
+
+tar -c -T /dev/null -z -f "$_REPO_LOCAL_PATH/$F_alias.tar.gz"
+echo "alias" > "$_REPO_LOCAL_PATH/$F_alias.version"
+echo "$F_target" > "$_REPO_LOCAL_PATH/$F_alias.dependencies"
+touch "$_REPO_LOCAL_PATH/$F_alias.done"
+
+echo "$(basename "$0"): aliased '$F_target' as '$F_alias'" >&2
+echo "$(basename "$0"): installing '$F_alias' will install '$F_target'" >&2
