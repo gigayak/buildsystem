d68fc8dc24a1667bbbd414fc4105e42b8e78c2e2
John Gilik <john@jgilik.com>
2015-12-07 08:54:47 -0800

    Allow bootstrap packages to declare dependencies.
    This allows the SSL client certificate export package
    "stage2-certificate" to depend on the certificate authority export
    package "internal-ca-certificates", so that the CA certificate
    does not need to be copied around into multiple places.

--- pkg.from_bootstrap.sh
+++ pkg.from_bootstrap.sh
@@ -18,7 +18,12 @@ fi
 make_temp_dir tmprepo
 "$bootstrap" > "$tmprepo/$F_pkg_name.tar.gz"
 echo 1.0 > "$tmprepo/$F_pkg_name.version"
-touch "$tmprepo/$F_pkg_name.dependencies"
+if [[ -e "$DIR/pkgspecs/$F_pkg_name.deps.sh" ]]
+then
+  "$DIR/pkgspecs/$F_pkg_name.deps.sh" > "$tmprepo/$F_pkg_name.dependencies"
+else
+  touch "$tmprepo/$F_pkg_name.dependencies"
+fi
 touch "$tmprepo/$F_pkg_name.done"
 
 for n in tar.gz version dependencies done
