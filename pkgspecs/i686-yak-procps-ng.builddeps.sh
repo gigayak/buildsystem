29b1c109edcfbe8d3587f35884c3faa1a20ec389
John Gilik <john@jgilik.com>
2015-12-18 07:42:32 -0800

    Use final autoconf chain when building procps-ng.
    Trying to build a late-stage2 autoconf/automake/etc wound up
    being crossed off from the map as "here there be dragons" -
    but somehow these dependencies on the late-stage2 packages
    remained.

--- pkgspecs/i686-yak-procps-ng.builddeps.sh
+++ pkgspecs/i686-yak-procps-ng.builddeps.sh
@@ -3,9 +3,9 @@ set -Eeo pipefail
 source "$BUILDTOOLS/all.sh"
 dep i686-tools2-wget
 dep i686-tools2-tar
-dep i686-tools3-pkg-config-lite
-dep i686-tools3-autoconf
-dep i686-tools3-automake
-dep i686-tools3-gettext
-dep i686-tools3-libtool
+dep i686-yak-pkg-config-lite
+dep i686-yak-autoconf
+dep i686-yak-automake
+dep i686-yak-gettext
+dep i686-yak-libtool
 dep i686-yak-gcc
