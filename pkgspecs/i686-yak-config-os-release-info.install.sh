8877ffa7fbf267c938993102f1747863dee44cea
John Gilik <john@jgilik.com>
2016-01-19 07:46:04 -0800

    Enable HOST_OS detection by adding /etc/os-release file.
    This is a first baby-step to bootstrapping, as it begins to allow
    for dependendency translation to function within Gigayak.

---
+++ pkgspecs/i686-yak-config-os-release-info.install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -Eeo pipefail
+cat > /etc/os-release <<'EOF'
+NAME="Gigayak Linux"
+ID=i686-yak
+ID_LIKE=yak
+PRETTY_NAME="Gigayak Linux"
+ANSI_COLOR="0;36"
+HOME_URL="http://linux.gigayak.com/"
+SUPPORT_URL="http://linux.gigayak.com/"
+BUG_REPORT_URL="http://linux.gigayak.com/"
+EOF
+# TODO: Add support / bug report URLs.
