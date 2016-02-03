gigayak/buildsystem

commit 1f0c0439070ce202dd8682acade07c5f18af2cea

Obey target arch/distro flags.

pkg.from_name.sh
@@ -14,7 +14,10 @@ parse_flags "$@"
 
 host_distro="$("$(DIR)/os_info.sh" --distribution)"
 host_arch="$("$(DIR)/os_info.sh" --architecture)"
-dep="$(qualify_dep "$host_arch" "$host_distro" "$F_pkg_name")"
+dep="$(qualify_dep \
+  "$F_target_architecture" \
+  "$F_target_distribution" \
+  "$F_pkg_name")"
 arch="$(dep2arch "$host_arch" "$host_distro" "$dep")"
 distro="$(dep2distro "$host_arch" "$host_distro" "$dep")"
 name="$(dep2name "$host_arch" "$host_distro" "$dep")"

commit 57fb1fc32fcaac75e8fad4ea8279b4c0c9ddb3e4

Obey default arch/distro of host arch/distro.
Fixes a breakage from commit 1f0c0439.

pkg.from_name.sh
@@ -14,12 +14,17 @@ parse_flags "$@"
 
 host_distro="$("$(DIR)/os_info.sh" --distribution)"
 host_arch="$("$(DIR)/os_info.sh" --architecture)"
-dep="$(qualify_dep \
-  "$F_target_architecture" \
-  "$F_target_distribution" \
-  "$F_pkg_name")"
-arch="$(dep2arch "$host_arch" "$host_distro" "$dep")"
-distro="$(dep2distro "$host_arch" "$host_distro" "$dep")"
+arch="$F_target_architecture"
+if [[ -z "$arch" ]]
+then
+  arch="$host_arch"
+fi
+distro="$F_target_distribution"
+if [[ -z "$distro" ]]
+then
+  distro="$host_distro"
+fi
+dep="$(qualify_dep "$arch" "$distro" "$F_pkg_name")"
 name="$(dep2name "$host_arch" "$host_distro" "$dep")"
 
 constraint_flags=()