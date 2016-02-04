5c7e53063603b5d4784fda7f56e07350dc60f95b
John Gilik <john@jgilik.com>
2016-02-03 17:50:09 -0800

    Use `find_localstorage.sh` to locate localstorage.

--- pkgspecs/tools-stage2-certificate.bootstrap.sh
+++ pkgspecs/tools-stage2-certificate.bootstrap.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -Eeo pipefail
-DIR(){(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)}
-
-# You'd think this would fail - but remember: bootstrap scripts are unique
-# in that they DON'T execute in a chroot.  Thus, we CAN import stuff from
-# the main buildsystem directory...
-source "$(DIR)/../cleanup.sh"
-
-make_temp_dir root
-ca_dir="$(DIR)/../../localstorage/certificate-authority/ca"
+arch="$TARGET_ARCH"
+root="$WORKSPACE/root"
+mkdir -p "$root/clfs-root/tools/$arch/etc/ssl/certs"
+localstorage="$("$BUILDSYSTEM/find_localstorage.sh")"
+ca_dir="$localstorage/certificate-authority/ca"
 key_dir="$root/clfs-root/opt/ssl"
 mkdir -p "$key_dir"
 cp \
@@ -17,8 +13,4 @@ cp \
 cp \
   "$ca_dir/certificates/client.system@stage2.automation.jgilik.com.crt" \
   "$key_dir/client.crt"
-# Link CA certificate from i686-tools-internal-ca-certificates
-ln -s \
-  "/tools/i686/etc/ssl/certs/machine-audio-research-ca.pem" \
-  "$key_dir/ca.crt"
 tar -cz -C "$root" .
