903ac7858bda75d1722943685e4283922214848d
John Gilik <john@jgilik.com>
2016-04-19 08:11:20 -0700

    Prevent ca-certificates from conflicting with itself when building... itself.

--- pkgspecs/yak-ca-certificates.install.sh
+++ pkgspecs/yak-ca-certificates.install.sh
@@ -2,5 +2,18 @@
 set -Eeo pipefail
 
 mkdir -pv /etc/ssl/certs
-cp "$YAK_WORKSPACE/certs/"*.pem /etc/ssl/certs/
-cat "$YAK_WORKSPACE/certs/"*.pem > /etc/ssl/ca-bundle.crt
+rm -f /etc/ssl/ca-bundle.crt
+touch /etc/ssl/ca-bundle.crt
+while read -r filename
+do
+  cp "$filename" /etc/ssl/certs/
+  cat "$filename" >> /etc/ssl/ca-bundle.crt
+
+  # Another ca-certificates package may have been present when building
+  # this package - so explicitly packaging all files prevents conflicts of
+  # file ownership.  This is okay, as the conflicts are with ca-certificates,
+  # and the only case in which two copies of that package are installed at
+  # once is this one: when building a new copy!
+  echo "/etc/ssl/certs/$(basename "$filename")" \
+    >> "$YAK_WORKSPACE/extra_installed_paths"
+done < <(find "$YAK_WORKSPACE/certs" -iname '*.pem')
