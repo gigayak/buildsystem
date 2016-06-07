3a384a3ed1ed645c22457306d12ff4d216fc1fad
John Gilik <john@jgilik.com>
2016-06-06 17:45:00 -0700

    Let git find /etc/passwd for whatever reason.

--- pkgspecs/git.deps.sh
+++ pkgspecs/git.deps.sh
@@ -6,3 +6,12 @@ dep curl
 dep expat
 dep perl
 dep tcl
+
+# git looks up the current user in the passwd file for some reason - without
+# this dependency, git clone fails with:
+#
+#     fatal: unable to look up current user in the passwd file: No such file
+#     or directory
+#
+# No, I'm not entirely sure why.
+dep linux-credentials
