ab8360cc78473e48f3a1bc66a2878dc51c0acbbe
John Gilik <john@jgilik.com>
2016-06-20 20:32:37 -0700

    Avoid accidentally following symlinks in yak-bash-config install.
    This prevents a symlink such as /etc/shells ->
    /tools/$ARCH/etc/shells from making yak-bash-config create
    /tools/$ARCH/etc/shells instead of the intended /etc/shells.

--- pkgspecs/yak-bash-config.install.sh
+++ pkgspecs/yak-bash-config.install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -Eeo pipefail
 
+rm -f /etc/profile # in case it already exists as a symlink
 cat > /etc/profile <<'EOF'
 for f in /etc/bash_completion.d/* /etc/profile.d/*
 do
@@ -13,6 +14,7 @@ export LC_ALL=en_US.UTF-8
 EOF
 
 # TODO: make this work in a .d config directory...
+rm -f /etc/shells # in case it already exists as a symlink
 echo > /etc/shells <<'EOF'
 # valid login shells
 /bin/bash
