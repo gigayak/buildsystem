f7660f5bbb557490ed1e8a8aed2a75b7b94b4c29
John Gilik <john@jgilik.com>
2016-06-23 02:25:55 -0700

    Rename python2 dependency to python.

--- pkgspecs/yak-glib.deps.sh
+++ pkgspecs/yak-glib.deps.sh
@@ -3,4 +3,4 @@ set -Eeo pipefail
 source "$YAK_BUILDTOOLS/all.sh"
 dep --arch="$YAK_TARGET_ARCH" --distro=yak glibc
 dep --arch="$YAK_TARGET_ARCH" --distro=yak libffi
-dep --arch="$YAK_TARGET_ARCH" --distro=yak python2
+dep --arch="$YAK_TARGET_ARCH" --distro=yak python
