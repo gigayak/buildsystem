fd43568a2a3b3390d44f5423453c73c0b60828d3
John Gilik <john@jgilik.com>
2016-06-22 19:36:12 -0700

    Make it possible to build tar natively.

--- pkgspecs/yak-tar.builddeps.sh
+++ pkgspecs/yak-tar.builddeps.sh
@@ -2,6 +2,15 @@
 set -Eeo pipefail
 source "$YAK_BUILDTOOLS/all.sh"
 
-dep --arch="$YAK_TARGET_ARCH" --distro=tools2 wget
-dep --arch="$YAK_TARGET_ARCH" --distro=tools2 tar
+# If building as a part of stage2, don't assume that wget/tar are already
+# built, and instead use the tools wget/tar.
+if [[ -e /tools ]]
+then
+  dep --arch="$YAK_TARGET_ARCH" --distro=tools2 wget
+  dep --arch="$YAK_TARGET_ARCH" --distro=tools2 tar
+# Otherwise, try to aim for a clean build.
+else
+  dep --arch="$YAK_TARGET_ARCH" --distro=yak wget
+  dep --arch="$YAK_TARGET_ARCH" --distro=yak tar
+fi
 dep --arch="$YAK_TARGET_ARCH" --distro=yak gcc
