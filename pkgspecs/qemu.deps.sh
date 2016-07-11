0f4567966d969d6ee327f65697f25b5cbd18d9fe
John Gilik <john@jgilik.com>
2016-07-11 08:24:28 -0700

    Add surprise gnutls dependency to qemu.

--- pkgspecs/qemu.deps.sh
+++ pkgspecs/qemu.deps.sh
@@ -9,3 +9,8 @@ dep gcc # for libstdc++ :[
 # ./configure asked for headers, so assume we link the following:
 dep zlib
 dep glib
+
+# Picked up passively if not explicitly declared:
+# qemu-system-i386: error while loading shared libraries: libgnutls.so.30:
+# cannot open shared object file: No such file or directory
+dep gnutls
