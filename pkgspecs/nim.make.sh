0b8c51dcb3bd62bd03a1d743d88cda2909bfefda
John Gilik <john@jgilik.com>
2016-09-05 19:29:04 -0700

    Nim package, because I like playing with new languages.

---
+++ pkgspecs/nim.make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -Eeo pipefail
+source "$YAK_BUILDTOOLS/download.sh"
+cd "$YAK_WORKSPACE"
+version="0.14.2"
+echo "$version" > version
+url="http://nim-lang.org/download/nim-${version}.tar.xz"
+download "$url"
+tar -xf *.tar.*
+cd *-*/
+./build.sh
