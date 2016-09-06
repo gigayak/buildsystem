3de882017c5f0f01555682501f935aa2d58e5001
John Gilik <john@jgilik.com>
2016-09-05 19:31:16 -0700

    Provide mirror locations for dhcpcd in case primary is down.
    (It was down.)

--- pkgspecs/yak-dhcpcd.make.sh
+++ pkgspecs/yak-dhcpcd.make.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -Eeo pipefail
+source "$YAK_BUILDTOOLS/download.sh"
 
 cd "$YAK_WORKSPACE"
 version=6.9.4
 echo "$version" > "$YAK_WORKSPACE/version"
-url="http://roy.marples.name/downloads/dhcpcd/dhcpcd-${version}.tar.xz"
-wget "$url"
+urls=()
+file="dhcpcd-${version}.tar.xz"
+# Main site.
+urls+=("http://roy.marples.name/downloads/dhcpcd/$file")
+# And mirrors, in case the main site is down.
+urls+=("http://ftp.oregonstate.edu/.1/blfs/conglomeration/dhcpcd/$file")
+urls+=("http://ftp.lfs-matrix.net/pub/blfs/conglomeration/dhcpcd/$file")
+download_one_of "${urls[@]}"
 tar -xf *.tar.*
 
 cd *-*/
