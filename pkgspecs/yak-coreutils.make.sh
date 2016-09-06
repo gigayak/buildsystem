5163695fc8fdfc0b5942b82d56ba8bb9f9040ea7
John Gilik <john@jgilik.com>
2016-09-05 19:32:53 -0700

    Upgrade coreutils to 8.25, and migrate configure to array notation.

--- pkgspecs/yak-coreutils.make.sh
+++ pkgspecs/yak-coreutils.make.sh
@@ -2,7 +2,7 @@
 set -Eeo pipefail
 
 cd "$YAK_WORKSPACE"
-version=8.23
+version=8.25
 echo "$version" > "$YAK_WORKSPACE/version"
 url="http://ftp.gnu.org/gnu/coreutils/coreutils-$version.tar.xz"
 wget "$url"
@@ -10,13 +10,20 @@ wget "$url"
 tar -Jxf "coreutils-$version.tar.xz"
 cd coreutils-*/
 
+configure_flags=()
+configure_flags+=(--prefix="/usr")
+configure_flags+=(--libexecdir="/usr/lib")
+configure_flags+=(--enable-no-install-program="kill,uptime")
+configure_flags+=(--enable-install-program="hostname")
+
+configure_command=()
 # FORCE_UNSAFE_CONFIGURE makes configure run as uid 0, as it has an
 # explicit check to see if you're compiling as root.
-FORCE_UNSAFE_CONFIGURE=1 \
-./configure \
-  --prefix=/usr \
-  --libexecdir=/usr/lib \
-  --enable-no-install-program=kill,uptime \
-  --enable-install-program=hostname
+configure_command+=(env FORCE_UNSAFE_CONFIGURE=1)
+configure_command+=(./configure)
+configure_command+=("${configure_flags[@]}")
+
+echo "Configuring with ${configure_command[*]}" >&2
+"${configure_command[@]}"
 
 make
