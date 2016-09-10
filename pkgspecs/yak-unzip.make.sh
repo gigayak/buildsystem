a07c274a50e9b3df8dfe05ae37c651738ca6b882
John Gilik <john@jgilik.com>
2016-09-10 12:52:52 -0700

    Add package spec for unzip.

---
+++ pkgspecs/yak-unzip.make.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Eeo pipefail
+source "$YAK_BUILDTOOLS/download.sh"
+cd "$YAK_WORKSPACE"
+version="6.0"
+echo "$version" > version
+major_version="$(echo "$version" | sed -re 's@\.[^\.]+$@\.x@g')"
+nodot_version="$(echo "$version" | tr -d '.')"
+urldir="infozip/UnZip%20${major_version}%20%28latest%29/UnZip%20${version}"
+url="$urldir/unzip${nodot_version}.tar.gz"
+download_sourceforge "$url"
+tar -xf *.tar.*
+cd */
+unix/configure --prefix=/usr
+make
