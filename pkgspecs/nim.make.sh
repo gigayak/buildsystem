8fb09c1e766ee81d64cff9ca21a191fa7d6457d8
John Gilik <john@jgilik.com>
2016-12-05 21:42:16 -0800

    Rote update of nim to latest version.

--- pkgspecs/nim.make.sh
+++ pkgspecs/nim.make.sh
@@ -2,7 +2,7 @@
 set -Eeo pipefail
 source "$YAK_BUILDTOOLS/download.sh"
 cd "$YAK_WORKSPACE"
-version="0.14.2"
+version="0.15.2"
 echo "$version" > version
 url="http://nim-lang.org/download/nim-${version}.tar.xz"
 download "$url"
