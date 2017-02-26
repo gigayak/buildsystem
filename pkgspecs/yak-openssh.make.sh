12a422429b8a8867f3aca23e66cffa69bbc7648d
John Gilik <john@jgilik.com>
2017-02-26 01:25:23 +0000

    Routine update of OpenSSH.

--- pkgspecs/yak-openssh.make.sh
+++ pkgspecs/yak-openssh.make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeo pipefail
 cd "$YAK_WORKSPACE"
-version="7.2p2"
+version="7.4p1"
 echo "$version" > version
 urldir="http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable"
 url="${urldir}/openssh-${version}.tar.gz"
