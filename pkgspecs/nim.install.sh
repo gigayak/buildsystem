83b1f8a67e9beba201390bcee4345d89302bab80
John Gilik <john@jgilik.com>
2017-04-23 04:36:41 +0000

    Update to Nim 0.16.0 and use Github release artifacts.

--- pkgspecs/nim.install.sh
+++ pkgspecs/nim.install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -Eeo pipefail
-cd "$YAK_WORKSPACE"/*-*/
-./install.sh /usr/bin
+cd "$YAK_WORKSPACE"/nim
+./koch install "/usr/bin"
+mkdir -pv "/usr/lib"
+cp -r lib "/usr/lib/nim"
+cp koch "/usr/bin/koch"
--- pkgspecs/nim.make.sh
+++ pkgspecs/nim.make.sh
@@ -2,10 +2,25 @@
 set -Eeo pipefail
 source "$YAK_BUILDTOOLS/download.sh"
 cd "$YAK_WORKSPACE"
-version="0.15.2"
+version="0.16.0"
 echo "$version" > version
-url="http://nim-lang.org/download/nim-${version}.tar.xz"
-download "$url"
-tar -xf *.tar.*
-cd *-*/
+url="https://github.com/nim-lang/Nim/archive/v${version}.tar.gz"
+wget -O nim.tar.gz "$url"
+
+cs_url="https://github.com/nim-lang/csources/archive/v${version}.tar.gz"
+wget -O csources.tar.gz "$cs_url"
+
+for pkg in *.tar.*
+do
+  tar -xf "$pkg"
+done
+mv Nim-*/ nim
+mv csources-*/ nim/csources
+
+cd nim/csources
 ./build.sh
+
+cd ..
+nim="$PWD/bin/nim"
+"$nim" c -d:release koch
+./koch boot -d:release
