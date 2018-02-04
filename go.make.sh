04072a43f94f39871ec6a24fe2ed790c012e8a8c
John Gilik <john@jgilik.com>
2018-02-04 03:06:08 +0000

    chore: add packages go-dep and go-govendor

--- go.make.sh
+++ go.make.sh
@@ -9,25 +9,36 @@ echo "Package name is: $pkg_name" >&2
 # Make sure go environment is sourced
 source /etc/profile.d/go.sh
 
+declare -A pkg_paths
+pkg_paths[govendor]="github.com/kardianos/govendor"
+pkg_paths[dep]="github.com/golang/dep/cmd/dep"
+
 export IMPORT_PATH=""
-paths=()
-paths+=("git.jgilik.com/$pkg_name")
-paths+=("github.com/gigayak/$pkg_name")
-for path in "${paths[@]}"
-do
-  if curl \
-    --head \
-    --fail \
-    "https://$path" \
-    >/dev/null
-  then
-    echo "Building from URL https://$path" >&2
-    export IMPORT_PATH="$path"
-    break
-  else
-    echo "Not considering URL https://$path" >&2
-  fi
-done
+if [[ ! -z "${pkg_paths[$pkg_name]}" ]]
+then
+  export IMPORT_PATH="${pkg_paths[$pkg_name]}"
+fi
+if [[ -z "$IMPORT_PATH" ]]
+then
+  paths=()
+  paths+=("git.jgilik.com/$pkg_name")
+  paths+=("github.com/gigayak/$pkg_name")
+  for path in "${paths[@]}"
+  do
+    if curl \
+      --head \
+      --fail \
+      "https://$path" \
+      >/dev/null
+    then
+      echo "Building from URL https://$path" >&2
+      export IMPORT_PATH="$path"
+      break
+    else
+      echo "Not considering URL https://$path" >&2
+    fi
+  done
+fi
 if [[ -z "$IMPORT_PATH" ]]
 then
   log_rote "Failed to find valid import path for '$pkg_name'"
